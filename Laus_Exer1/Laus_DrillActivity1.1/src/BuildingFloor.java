484884ab635052cf013295e4220e9a4c8ece8954
Angelo Laus <angelolausofcl@gmail.com>
2023-12-04 16:47:30 +0800

    feat: work on Exercise 1

---
+++ Laus_Exer1/Laus_DrillActivity1.1/src/BuildingFloor.java
@@ -0,0 +1,30 @@
+public class BuildingFloor {
+
+    double totalFloorArea;
+    int numberOfRooms;
+    double heightOfFloor;
+    String wallColor;
+
+    public BuildingFloor(double inputTotalFloorArea,
+                         int inputNumberOfRooms,
+                         double inputHeightOfFloor,
+                         String inputWallColor)
+
+    {
+
+        this.totalFloorArea = inputTotalFloorArea;
+        this.numberOfRooms = inputNumberOfRooms;
+        this.heightOfFloor = inputHeightOfFloor;
+        this.wallColor = inputWallColor;
+
+    }
+
+    public void makeFloorChanges(int newNumberOfRooms,
+                                 String newWallColor ) {
+
+        this.numberOfRooms = newNumberOfRooms;
+        this.wallColor = newWallColor;
+
+    }
+
+}
