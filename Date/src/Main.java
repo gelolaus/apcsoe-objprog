32741a605d4432fe69fcec3c51b4220c65a0905c
Angelo Laus <angelolausofcl@gmail.com>
2023-12-07 15:49:14 +0800

    feat: add Date

--- Date/src/Main.java
+++ Date/src/Main.java
@@ -8,14 +8,21 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
+        String title = "\n\n\t\t\t\t\tSimple Date Format\n\n";
         Date today = new Date();
         SimpleDateFormat sdf1, sdf2;
         sdf1 = new SimpleDateFormat("MM/dd/yy"); sdf2 = new SimpleDateFormat("MMMM dd, yyyy");
 
-        System.out.println("\n\nSimple Date Format");
+        System.out.println(title.toUpperCase());
+
+
         System.out.println("Day, Date, and Time Today:\t\t" + today.toString()); // Shows the full date without any formatting
         System.out.println("MM/DD/yy Format:\t\t\t\t" + sdf1.format(today)); // Shows the month, date, and year with the format MM/DD/yy
         System.out.println("MMMM dd, yyyy Format:\t\t\t" + sdf2.format(today)); // Shows the month, date, and year with the format MMM dd, yyyy
+
+        System.out.println("--------------------------------------------------------------");
+
+        System.out.println();
 
 
     }
