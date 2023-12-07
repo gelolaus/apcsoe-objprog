f8228232196dc62186856bf5c3a066526422a135
Angelo Laus <angelolausofcl@gmail.com>
2023-12-07 13:58:45 +0800

    feat: add prev topics review

---
+++ MyWindow/src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame myWindow = new JFrame();
+
+        myWindow.setSize(300, 200);
+        myWindow.setTitle("Hello, World!");
+        myWindow.setVisible(true);
+
+        myWindow.setResizable(false);
+        myWindow.setAlwaysOnTop(true);
+    }
+
+}
