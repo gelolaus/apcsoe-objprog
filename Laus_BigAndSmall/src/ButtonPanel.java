30ec2b893ea716c9f4e2bb4a578f406dbe1869d0
Angelo Laus <arlaus@student.apc.edu.ph>
2024-01-04 17:08:37 +0800

    feat: Big And Small Activity pt.4

--- Laus_BigAndSmall/src/ButtonPanel.java
+++ Laus_BigAndSmall/src/ButtonPanel.java
@@ -4,15 +4,16 @@ import java.awt.*;
 public class ButtonPanel {
 
     JPanel buttonPanel;
+    JButton rollButton;
+    JButton exitButton;
 
     public ButtonPanel() {
 
         buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout());
 
-        //rollButton, exitButton
-        JButton rollButton = new JButton("Roll");
-        JButton exitButton = new JButton("Exit");
+        rollButton = new JButton("Roll");
+        exitButton = new JButton("Exit");
 
         buttonPanel.add(rollButton);
         buttonPanel.add(exitButton);
--- Laus_BigAndSmall/src/DicePanel.java
+++ Laus_BigAndSmall/src/DicePanel.java
@@ -4,29 +4,27 @@ import java.awt.*;
 public class DicePanel {
 
     JPanel dicePanel;
-    Image dice1, dice2, dice3, dice4, dice5, dice6;
+    ImageIcon dice1, dice2, dice3, dice4, dice5, dice6;
     JLabel diceLabel;
 
     public DicePanel() {
 
     dicePanel = new JPanel();
     dicePanel.setLayout(null);
+    dicePanel.setBorder(BorderFactory.createLineBorder(Color.black));
     dicePanel.setBounds(0, 0, 150, 150);
 
-    dice1 = new ImageIcon("src/Images/rsz_dice_one.png").getImage();
-    dice2 = new ImageIcon("src/Images/rsz_dice_two.png").getImage();
-    dice3 = new ImageIcon("src/Images/rsz_dice_three.png").getImage();
-    dice4 = new ImageIcon("src/Images/rsz_dice_four.png").getImage();
-    dice5 = new ImageIcon("src/Images/rsz_dice_five.png").getImage();
-    dice6 = new ImageIcon("src/Images/rsz_dice_six.png").getImage();
+        dice1 = new ImageIcon("Laus_BigAndSmall/images/dice1.png");
+        dice2 = new ImageIcon("Laus_BigAndSmall/images/dice2.png");
+        dice3 = new ImageIcon("Laus_BigAndSmall/images/dice3.png");
+        dice4 = new ImageIcon("Laus_BigAndSmall/images/dice4.png");
+        dice5 = new ImageIcon("Laus_BigAndSmall/images/dice5.png");
+        dice6 = new ImageIcon("Laus_BigAndSmall/images/dice6.png");
 
-    dicePanel.setBorder(BorderFactory.createEtchedBorder());
-
-    diceLabel = new JLabel();
-    diceLabel.setIcon(new ImageIcon("src/Images/rsz_dice_one.png"));
-
-    dicePanel.add(diceLabel);
-
+        diceLabel = new JLabel();
+        diceLabel.setIcon(dice1);
+        diceLabel.setBounds(0, 0, 150, 150);
+        dicePanel.add(diceLabel);
 
 
     }
